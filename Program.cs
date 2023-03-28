7b4ab27f280828eeea02c493a76f077f3ede9dc4
Анатолий <Chepanov199287@gmail.com>
2023-03-28 21:06:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_4_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+            User.Login = "12345";
+            User.LoginLength = Convert.ToInt32(User.Login.Length);
+
+            Console.WriteLine("Длина логина {0}", User.LoginLength);
+            Console.ReadKey();
+        }
+    }
+}
